a24961c9f17281bff2344d373b8fb4d602f300b2
MateoMor <mateomorales5558@gmail.com>
2024-07-12 21:43:12 -0500

    contexto implementado

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,19 +1,20 @@
 "use client";
 
-import React, { useState } from 'react';
-import Component from '@/components/component/profile';
+import React, { useState } from "react";
+import Component from "@/components/component/profile";
 /* import { string } from 'zod'; */
 
 const ProfilePage = () => {
   const [userData, setUserData] = useState({
-    username: 'John Doe' ,
-    email: 'jhondoe@gmail.com',
-    password: '',
+    username: "John Doe",
+    email: "jhondoe@gmail.com",
+    password: "",
   });
+
+  /* const { token, setToken } = useAppContext() as AppContextType; */
 
   return (
     <div className="flex items-center justify-center min-h-screen w-full bg-background text-foreground">
-    
       <Component user={userData} setUser={setUserData} />
     </div>
   );
---
+++ src/types/task.ts
@@ -0,0 +1,4 @@
+export interface AppContextType {
+    token: string;
+    setToken: React.Dispatch<React.SetStateAction<string>>;
+  }
